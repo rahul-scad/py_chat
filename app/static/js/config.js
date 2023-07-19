981150928045238fd52581a7782103b01f59f384
rahul-scad <rkece5897643@gmail.com>
2023-07-19 12:43:07 +0530

    first commit

---
+++ app/static/js/config.js
@@ -0,0 +1,6 @@
+const API_URL = "https://api.openai.com/v1/chat/completions";
+// const API_URL_IMAGE = "https://api.openai.com/v1/images/generations";
+
+const API_KEY = ""    //Add your API_KEY
+const system_message = "You are a smart robot!"; // You can change this message
+const path = "/chat";  
---
+++ app/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from .views import app
+
+urlpatterns = [
+    
+    path("", app , name="app"),
+
+]
